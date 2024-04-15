803505c638542322c4fb0e19b70b086cebca79aa
Gregor Heinrich <gregor@arbylon.net>
2024-04-15 17:28:53 +0200

    fix product vision css, add version to app signature

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const defaultLsKey = 'preseedcanvas';
 const defaultModel = 'template';
 const configsFile = 'configs.json';
 const defaultConfigName = 'preseed';
-const appSignature = 'Unlost Canvas App';
+const appSignature = 'Unlost Canvas App v1.3.3';
 
 document.addEventListener('DOMContentLoaded', () => {
     const param = (key) => new URLSearchParams(window.location.search).get(key);
